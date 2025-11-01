8fbc54f3b13e040b3730b1822ee5afc93980a50a
DudarIg <zerolin65@gmail.com>
2025-11-01 15:41:49 +0300

    1 commit

---
+++ boot_start/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BootStartConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'boot_start'
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('bboard/', include('bboard.urls', namespace='bboard')),
+    path('boot_start/', include('boot_start.urls', namespace='boot_start')),
 ]
